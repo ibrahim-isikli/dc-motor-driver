d2124a0ef8fc6e34812526884c33f43a0a9e8d88
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2026-05-22 20:29:48 +0300

    FEAT added typedef and status of motor
    - Added motor_state_t and motor_t struct in dc_motor_driver.h

--- STM32/dc_motor_driver/dc_motor_driver/inc/dc_motor_driver.h
+++ STM32/dc_motor_driver/dc_motor_driver/inc/dc_motor_driver.h
@@ -5,6 +5,41 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// motor direction macros
+#define MOTOR_FORWARD	0u
+#define MOTOR_BACKWARD  1u
+
+
+// motor state
+typedef enum
+{
+	MOTOR_STATE_IDLE = 0u,
+	MOTOR_STATE_RUNNING,
+	MOTOR_STATE_FAULT
+}Motor_State_t;
+
+typedef struct
+{
+	// hardware abstraction layer references
+	TIM_HandleTypeDef*	htim;
+	uint32_t			tim_channel;
+
+	// direction gpio
+	GPIO_TypeDef		dir_port;
+	uint16_t			dir_pin;
+
+	// states
+	uint16_t			speed;
+	uint8_t				direction;
+	Motor_State_t		state;
+}Motor_t;
+
+
+//------------ API ------------
+
+void motor_init(Motor_t* self, TIM_HandleTypeDef* htim, uint32_t channel, GPIO_TypeDef* dir_port, uint16_t dir_pin);
+void motor_forward(Motor_t* self, uint16_t speed);
+
 
 
 #endif
